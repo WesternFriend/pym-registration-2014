a380fc320ed45a86a35f84f813aa94b5a9f80e41
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-02-12 21:41:53 -0800

    Added scaffold structure.

---
+++ script.js
@@ -0,0 +1,58 @@
+cj(function($) {
+    // Declare CSS variables
+        var commuter_adult_days = ".Commuter_Adult_Days-section",
+            commuter_youth_young_friend_days = ".Commuter_Youth_Young_Friend_Day-section",
+            commuter_child_days = ".Commuter_Child_Days-section",
+            commuter_infant_to_5_days = ".Commuter_Infant_to_5_Days-section",
+            overnight_adult_full_week = ".Overnight_Adult_Full_Week-section",
+            overnight_adult_daily_dorm = ".Overnight_Adult_Daily_Dorm_Qty-section",
+            overnight_adult_daily_dorm_days = ".Overnight_Adult_Daily_Dorm_Days-section",
+            overnight_adult_daily_semi_private = ".Overnight_Adult_Daily_Semi_priv-section",
+            overnight_adult_daily_camping_qty = ".Overnight_Adult_Daily_Camping_Q-section",
+            overnight_adult_daily_camping_days = ".Overnight_Adult_Daily_Camping_D-section",
+            overnight_young_friend_full_week = ".vernight_Young_Friend_Full_Wee-section",
+            overnight_young_friend_daily_dorm = ".Overnight_Young_Friend_Daily_Do-section",
+            overnight_young_friend_daily_semi_private = ".Overnight_Young_Friend_Daily_Se-section",
+            overnight_young_friend_daily_camping = ".Overnight_Young_Friend_Daily_Ca-section",
+            overnight_young_friend_daily_yf_area = ".Overnight_Young_Friend_Daily_YF-section",
+            overnight_youth_full_week = ".Overnight_Youth_Full_Week-section",
+            overnight_youth_daily_dorm_qty = ".Overnight_Youth_Daily_Dorm_Qty-section",
+            overnight_youth_daily_dorm_days =".Overnight_Youth_Daily_Dorm_Days-section",
+            overnight_youth_daily_semi_private = ".Overnight_Youth_Daily_Semi_priv-section",
+            overnight_youth_daily_camping_qty = ".rm-section Overnight_Youth_Daily_Camping_Q-section",
+            overnight_youth_daily_camping_days = ".Overnight_Youth_Daily_Camping_D-section",
+            overnight_youth_daily_jym_area = ".Overnight_Youth_Daily_JYM_area_-section",
+            overnight_child_full_week = ".Overnight_Child_Full_Week-section",
+            overnight_child_daily_dorm_qty = ".Overnight_Child_Daily_Dorm_Qty-section",
+            overnight_child_daily_dorm_days = ".Overnight_Child_Daily_Dorm_Days-section",
+            overnight_child_daily_camping_qty = ".Overnight_Child_Daily_Camping_Q-section",
+            overnight_child_daily_camping_days = ".Overnight_Child_Daily_Camping_D-section",
+            overnight_infant_to_five_full_week = ".Overnight_Infant_to_5_Full_Week-section",
+            overnight_infant_to_five_daily_dorm = ".Overnight_Infant_to_5_Daily_Dor-section",
+            overnight_infant_to_five_daily_semi_private = ".Overnight_Infant_to_5_Daily_Sem-section",
+            overnight_infant_to_five_daily_camping = ".Overnight_Infant_to_5_Daily_Cam-section",
+            overnight_optional_add_ons = ".Overnight_Optional_add_ons-section";
+    
+    $(document).ready(function() {
+        // Hide all form price elements
+        hideAllPrices();
+        
+        // Attach event listeners to all form price elements
+        attachEventListeners();
+    });
+    
+    var hideAllPrices = function () {
+        // Code to hide all price sections
+    };
+    
+    var showPriceSection = function (sectionVariable) {
+        // Code to show a price section
+    };
+    var resetAllPriceValues = function () {
+        // reset all price values. e.g. triggered when form accommodations change.
+    };
+    
+    var attachEventListeners = function () {
+        // Attach event listeners to all page elements
+    };
+}) (jQuery);
